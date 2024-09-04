7fa9d59fab7e6eb9ea18a036730fbdcc6c08e7de
manu05X <k.manu00005@gmail.com>
2024-09-04 18:20:19 +0530

    Adding  Async/Async/asyncOperationSerial.js

---
+++ Async/Async/fileOperationSerial.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+console.log("before");
+
+fs.readFile("f1.txt", cb1);
+
+//Making above synchronous calls with callback by calling the next callback inside the callback function itself
+
+function cb1(err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("The File data ->" + data);
+  fs.readFile("f2.txt", cb2);
+}
+
+function cb2(err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("The File 2 data ->" + data);
+  fs.readFile("f3.txt", cb3);
+}
+
+function cb3(err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("The File 3 data ->" + data);
+}
+
+console.log("after");
