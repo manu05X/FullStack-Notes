df45da3cb43ab06d0ddce9395dcb7411558bec40
manu05X <k.manu00005@gmail.com>
2024-09-04 19:20:07 +0530

    Adding  Promoise2,3

--- Async/Promise/promise2.js
+++ Async/Promise/promise2.js
@@ -29,6 +29,49 @@ console.log("before");
 
 //________________________________________________________________
 
+// let promiseReadFile1 = fs.promises.readFile("f1.txt");
+
+// promiseReadFile1.then(function (data1) {
+//   console.log("the file data is ->" + data1);
+// });
+
+// promiseReadFile1.catch(function (err) {
+//   console.log(err);
+// });
+
+// let promiseReadFile2 = fs.promises.readFile("f2.txt");
+
+// promiseReadFile2.then(function (data2) {
+//   console.log("the file data is ->" + data2);
+// });
+
+// promiseReadFile2.catch(function (err) {
+//   console.log(err);
+// });
+
+// let promiseReadFile3 = fs.promises.readFile("f3.txt"); // this will give error as no f3.txt file exists
+// // let promiseReadFile3 = fs.promises.readFile("f2.txt");
+
+// // Error handling is not graceful here. The catch block is not just after then so it exits abnormally
+// promiseReadFile3.then(function (data2) {
+//   console.log("the file data is ->" + data2);
+// });
+
+// promiseReadFile3.catch(function (err) {
+//   console.log(err);
+// });
+
+// Error handling gracefully as soon as the file is not found the catch block will be executed
+// promiseReadFile3
+//   .then(function (data1) {
+//     console.log("the file data 3 is ->" + data1);
+//   })
+//   .catch(function (err) {
+//     console.log("Error Statement -> " + err);
+//   });
+
+//________________________________________________________________
+
 let promiseReadFile1 = fs.promises.readFile("f1.txt");
 let promiseReadFile2 = fs.promises.readFile("f2.txt");
 let promiseReadFile3 = fs.promises.readFile("f3.txt");
@@ -44,6 +87,7 @@ function handleError(err) {
 promiseReadFile1.then(readFileCallback).catch(handleError);
 promiseReadFile2.then(readFileCallback).catch(handleError);
 promiseReadFile3.then(readFileCallback).catch(handleError);
+
 /*
 OUTPUT: 
 before
