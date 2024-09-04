01e0949aca8eeb11f391f0f401b2d2ff63637eb9
manu05X <k.manu00005@gmail.com>
2024-09-04 20:12:46 +0530

    Adding  Promoise5

--- Async/Promise/promise4.js
+++ Async/Promise/promise4.js
@@ -1,54 +1,131 @@
 // What is this async Keyword
 
 // async keyword always returns a Promise
-async function greeting() {
-  return "Hello"; // 1st case
-}
+// async function greeting() {
+//   return "Hello"; // 1st case
+// }
 
-let message = greeting();
+// let message = greeting();
 
-message.then((res) => console.log(res));
+// message.then((res) => console.log(res));
 
-console.log(message);
+// console.log(message);
 
+//________________________________________________________________
 // New Promise
 
-// let newPromise = new Promise((resolve , reject)=>{
-//     setTimeout(()=>{
-//         resolve('Promise Resolved successfully')
-//     } , 40000)
-// })
+// let newPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise Resolved successfully");
+//   }, 4000);
+// });
 
-// let newPromise2 = new Promise((resolve , reject)=>{
-//     setTimeout(()=>{
-//         resolve('Promise 2 Resolved Successfully')
-//     } , 60000)
-// })
+// function executePromise() {
+//   newPromise.then((res) => console.log(res));
 
-// function executePromise(){
-//     newPromise.then((res)=>console.log(res))
-
-//     console.log('Scaler is Awesome!!')
+//   console.log("Promise is executePromise in this function!!");
 // }
+
+// executePromise();
+
+//____using await to resolve Promise____________________________________________________________
+
+// let newPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Promise 1 Resolved successfully");
+//   }, 4000);
+// });
+
+// function executePromise() {
+//   newPromise.then((res) => console.log(res));
+
+//   console.log("Promise is executePromise in this function!!");
+// }
+
+// async function executePromiseAsync() {
+//   const val1 = await newPromise; // waits here for 4 seconds for promise to execute then below statement is executed
+//   console.log(val1);
+//   console.log("Promise is executePromiseAsync using await");
+// }
+
+//executePromise();
+
+/*
+Promise is executePromise in this function!!
+// after 4 seconds below is printed
+Promise Resolved successfully
+*/
+
+// executePromiseAsync();
+/*
+//Both the below is printed adter 4seconds at same time
+Promise Resolved successfully
+Promise is executePromiseAsync using await
+
+*/
+
+//____using await to resolve two Promise to see the effect____________________________________________________________
+
+let newPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise 1 Resolved successfully");
+  }, 4000);
+});
+
+let newPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise 2 Resolved Successfully");
+  }, 6000);
+});
+
+// function executePromise() {
+//   newPromise.then((res) => console.log(res));
+
+//   console.log("Promise is executePromise in this function!!");
+// }
+
+// async function executePromiseAsync() {
+//   const val1 = await newPromise; // waits here for 4 seconds for promise to execute then below statement is executed
+//   console.log(val1);
+
+//   const val2 = await newPromise2;
+//   console.log(val2);
+//   console.log("Promise is executePromiseAsync using await");
+// }
+
+// //executePromise();
+
+// /*
+// Promise is executePromise in this function!!
+// // after 4 seconds below is printed
+// Promise Resolved successfully
+// */
+
+// executePromiseAsync();
+/*
+//Both the below is printed adter 4seconds at same time
+Promise Resolved successfully
+Promise is executePromiseAsync using await
+
+*/
 
 // //Scaler is Awesome
 // //Promise resolved messaseg after 4 secs
 
-// async function  executePromiseAsync(){
+async function executePromiseAsync() {
+  console.log("Hello");
 
-//      console.log('Hello')
+  const val1 = await newPromise; // suspended
+  console.log(val1); // 8
 
-//      const val1 = await newPromise // suspended
-//      console.log(val1) // 8
+  const val2 = await newPromise2;
+  console.log(val2); // 12
 
-//      const val2 = await newPromise2
-//      console.log(val2) // 12
+  //  both after 6 secs
 
-//     //  both after 6 secs
+  console.log("Scaler is Awesome from Async Function");
+}
+console.log("Statment outside async function");
+//executePromise(); // output
 
-//      console.log('Scaler is Awesome from Async Function')
-// }
-
-//  executePromise() // output
-
-//  executePromiseAsync()
+executePromiseAsync();
